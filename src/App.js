4a5ca51fffe1b6203a43645eea474f88ea96a25b
Maha Mahmood <>
2020-12-16 21:56:56 -0500

    created boxes container to manage colors state.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Box from './Box';
 import './App.css';
+import BoxesContainer from './BoxesContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h2>Hello World</h2>
-        <Box />
+        <BoxesContainer />
       </div>
     );
   }
--- src/Box.js
+++ src/Box.js
@@ -3,18 +3,15 @@ import './Box.css';
 import { choice } from './helpers';
 
 class Box extends Component {
-    static defaultProps = {
-        allColor: ['purple', 'magenta', 'violet', 'pink']
-    }
     constructor(props) {
         super(props);
-        this.state = { color: choice(this.props.allColor)};
+        this.state = { color: choice(this.props.colors)};
         this.handleClick = this.handleClick.bind(this);
     }
     pickColor() {
         let newColor;
         do {
-            newColor = choice(this.props.allColor);
+            newColor = choice(this.props.colors);
         } while (newColor === this.state.color);
         
         this.setState({ color: newColor });
